35338e6e96912fc77d1c465901363b6de65bc3bf
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2024-01-11 10:58:38 +0100

    EPIBOOK STARTED

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter'
+import Welcome from './components/Welcome'
+// import AllTheBooks from './components/AllTheBooks'
+import { Container } from 'react-bootstrap'
+import BookList from './components/BookList'
 
-import './App.css';
+import fantasy from './data/fantasy.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    <>
+      <MyNav />
+      <Container>
+        <Welcome />
+        {/* <AllTheBooks /> */}
+        <BookList books={fantasy} />
+      </Container>
+      <MyFooter />
+    </>
+  )
 }
 
-export default App;
+export default App
